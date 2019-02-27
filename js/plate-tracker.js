0a70261e12a1242655fa1156d48eb913d5d0a11a
Addison Moore <addisondrewmoore@gmail.com>
2019-02-27 14:18:55 -0800

    parceled site, added carousel to Plate Tracker

---
+++ js/plate-tracker.js
@@ -0,0 +1,9 @@
+var $ = require("jquery");
+require("slick-carousel");
+$(document).ready(function() {
+  $(".plate-tracker-slider").slick({
+    slidesToShow: 5,
+    dots: true,
+    centerMode: true
+  });
+});
