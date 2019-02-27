bb3cde5e6b8e5d010dcd19e1a97799e981675b51
Addison Moore <addisondrewmoore@gmail.com>
2019-02-27 15:19:18 -0800

    upddated styling

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.getElementById("clippy").addEventListener("mouseup", copyEmail);
 
 function copyEmail() {
   let myEmail = document.getElementById("myEmail");
-  myEmail.innerHTML = "Copied to clipboard!";
+  myEmail.innerHTML = "<span id='copied'>Copied to clipboard!<span>";
   let reset = setInterval(() => {
     myEmail.innerHTML != "Copied to clipboard!"
       ? window.clearInterval(reset)
