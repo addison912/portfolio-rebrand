ace04a335bc563f693b4d8b0faee9021b85852c7
Addison Moore <addisondrewmoore@gmail.com>
2021-03-22 00:14:24 -0700

    updated build

---
+++ build/freelance.74a6fa37.js
@@ -0,0 +1,2 @@
+!function(){var e=(0,("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire1a0d)("pWBic");e(),new(e().default)("#freelance-carousel",{type:"carousel",focusAt:"center",perView:1}).mount()}();
+//# sourceMappingURL=freelance.74a6fa37.js.map
